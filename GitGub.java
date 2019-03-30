034b8118e8228f82ccdebc39421eaa3e20031d6e
puneet <puneet.phpdeveloper@gmail.com>
2019-03-30 11:34:05 +0530

    Add constrcutor

--- GitGub.java
+++ GitGub.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class BasicAray {
-
+	
+	public void BasicAray()
+	{
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		int arr[] = {10,20,30,40,50,60,70,80};
